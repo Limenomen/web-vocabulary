998af36c9fdf259623a099738660187eb1c3127e
rsagadatov <Rsagadatov@gmail.com>
2023-01-08 22:20:40 +0500

    изменение порядка отображения записей

--- core/forms.py
+++ core/forms.py
@@ -4,7 +4,7 @@ from core import models
 
 class ArticleSearch(forms.Form):
     name = forms.CharField(label='Название', required=False)
-    tag = forms.ModelMultipleChoiceField(label='теги', queryset=models.Tag.objects.all(), required=False)
+    tag = forms.ModelMultipleChoiceField(label='теги', queryset=models.Tag.objects.order_by('-id'), required=False)
 
 
 class Article(forms.ModelForm):
